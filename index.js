294124db2c58349b6d35d9a67128bfec7d62918f
costin2121 <dobrecostindragos@gmail.com>
2025-02-25 00:41:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const video = document.getElementById("video");
+const h1 = document.getElementById("h");
+const input = document.getElementById("solutia");
+
+setTimeout(() => {
+    video.pause();
+    h1.textContent = "Buna seara! Doamna Mihaita Boss, care este cuvantul?";
+    input.disabled = false;
+}, 23000);
+
+input.addEventListener("input", function () {
+    let value = input.value.toLowerCase().replace(/-/g, '').replace(/ /g, '');
+    if (value === "luamiaicoaieleingura" || value === "luamiaicuaieleingura") {
+        h1.textContent = "Felicitari!";
+        h1.style.color = "green";
+        video.play();
+        input.disabled = true;
+    } else {
+        h1.textContent = "Gresit! Mai incercati.";
+    }
+});
